7e963c26abbcb258f57cec905299b8a8153a383f
Jose Moreno <jose.morenoesteban@zooplus.com>
2017-01-05 16:19:32 +0100

    Add token refreshing and all fields to token

--- src/main/java/org/jomoespe/lab/jwt/sample/JwtHelper.java
+++ src/main/java/org/jomoespe/lab/jwt/sample/JwtHelper.java
@@ -4,9 +4,11 @@ import static io.jsonwebtoken.SignatureAlgorithm.RS256;
 
 import static java.time.Instant.now;
 import static java.time.Duration.ofSeconds;
+import static java.util.Date.from;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.impl.crypto.RsaProvider;
 
@@ -14,6 +16,7 @@ import java.security.Key;
 import java.security.KeyPair;
 import java.time.Duration;
 import java.util.Date;
+import java.util.function.Supplier;
 
 public class JwtHelper {
     static {
@@ -25,27 +28,45 @@ public class JwtHelper {
     
     private JwtHelper() { /* private constructor to avoid instantiation */ }
     
-    public static String token(final String id, final String subject) {
-        return Jwts.builder()
-            .signWith( RS256, PRIV )
-            .setId( id )
+    public static String token(final String subject, final String role) {
+        return baseJwt.get()
             .setSubject( subject )
-            .setExpiration( new Date(now().plus(EXPIRATION).toEpochMilli()) )
+            .claim("roles", role)
             .compact();
     }
 
+    public static String refresh(final String token) {
+        Jws<Claims> claims = claims( token );
+        return token( claims.getBody().getSubject(), claims.getBody().get("roles", String.class) );
+    }
+    
     public static Jws<Claims> claims(final String token) {
         return Jwts.parser()
             .setSigningKey( PUB )
             .parseClaimsJws( token );
     }
-
-    public static Duration expiration() {
+    
+    public static Duration duration() {
         return EXPIRATION;
     }
-    
-    private static final long     DEFAULT_EXPIRATION = 5;  // in seconds
-    private static final Duration EXPIRATION         = ofSeconds(DEFAULT_EXPIRATION);
+
     private static final Key      PRIV;
     private static final Key      PUB; 
+    
+    private static final String   JWT_ID     = "jwt id";
+    private static final String   ISSUER     = "jomoespe issuer";
+    private static final String   AUDIENCE   = "jomoespe jwt test";
+    private static final Duration EXPIRATION = ofSeconds( 5 );
+    
+    private static final Supplier<Date>       expiration = () -> from( now().plus( EXPIRATION ) );
+    private static final Supplier<Date>       notBefore  = () -> from( now() );
+    private static final Supplier<Date>       issuedAt   = () -> from( now() );
+    private static final Supplier<JwtBuilder> baseJwt    = () -> Jwts.builder()
+            .signWith( RS256, PRIV )
+            .setId( JWT_ID )
+            .setIssuer( ISSUER )
+            .setAudience( AUDIENCE )
+            .setExpiration( expiration.get() )
+            .setNotBefore( notBefore.get() )
+            .setIssuedAt( issuedAt.get() );
 }
--- src/test/java/org/jomoespe/lab/jwt/sample/JwtHelperTest.java
+++ src/test/java/org/jomoespe/lab/jwt/sample/JwtHelperTest.java
@@ -2,12 +2,14 @@ package org.jomoespe.lab.jwt.sample;
 
 import static org.jomoespe.lab.jwt.sample.JwtHelper.token;
 import static org.jomoespe.lab.jwt.sample.JwtHelper.claims;
-import static org.jomoespe.lab.jwt.sample.JwtHelper.expiration;
+import static org.jomoespe.lab.jwt.sample.JwtHelper.refresh;
+import static org.jomoespe.lab.jwt.sample.JwtHelper.duration;
 
 import static java.lang.Thread.currentThread;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
@@ -18,24 +20,47 @@ import org.junit.Test;
 public class JwtHelperTest {
     @Test
     public void assertCanWorkWithToken() {
-        String token = token("user_id@server.com", "Firstname Lastname");
+        String token = token("user_id@server.com", "one role");
         assertNotNull(token);
         
         Jws<Claims> claims = claims( token );
         assertNotNull(claims);
-        assertEquals("user_id@server.com",  claims.getBody().getId());
-        assertNotNull("Firstname Lastname", claims.getBody().getSubject());
+        assertEquals("user_id@server.com",  claims.getBody().getSubject());
+        assertEquals("one role",            claims.getBody().get("roles"));
     }
 
     @Test(expected = ExpiredJwtException.class)
     public void assertTokenExpires() {
-        String token = token("user_id@server.com", "Firstname Lastname");
+        String token = token("user_id@server.com", "one role");
         assertNotNull(token);
         
         try {
-            currentThread().sleep( expiration().getSeconds() * 1000 );
+            currentThread().sleep( duration().getSeconds() * 1000 );
         } catch (InterruptedException e) {} 
         
         Jws<Claims> claims = claims( token );
     }
+
+    @Test
+    public void assertcanRefreshAToken() {
+        String token = token("user_id@server.com", "one role");
+        try { currentThread().sleep( 4000 ); } catch (InterruptedException e) {} 
+
+        String refreshed = refresh( token );
+        try { currentThread().sleep( 4000); } catch (InterruptedException e) {} 
+        claims( refreshed );
+        assertNotEquals(token, refreshed);
+    }
+
+    @Test(expected = ExpiredJwtException.class)
+    public void assertCannotRefreshAnOutdatedToken() {
+        String token = token("user_id@server.com", "one role");
+        assertNotNull(token);
+        
+        try {
+            currentThread().sleep( duration().getSeconds() * 1000 );
+        } catch (InterruptedException e) {} 
+        refresh( token );
+    }
+
 }
