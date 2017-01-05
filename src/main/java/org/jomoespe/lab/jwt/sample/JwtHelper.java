dca3465d7fc0b4194a6ad584ced779e149a4f251
Jose Moreno <jose.morenoesteban@zooplus.com>
2017-01-05 09:25:16 +0100

    Add expiration to token

--- src/main/java/org/jomoespe/lab/jwt/sample/JwtHelper.java
+++ src/main/java/org/jomoespe/lab/jwt/sample/JwtHelper.java
@@ -2,6 +2,9 @@ package org.jomoespe.lab.jwt.sample;
 
 import static io.jsonwebtoken.SignatureAlgorithm.RS256;
 
+import static java.time.Instant.now;
+import static java.time.Duration.ofSeconds;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
@@ -9,6 +12,8 @@ import io.jsonwebtoken.impl.crypto.RsaProvider;
 
 import java.security.Key;
 import java.security.KeyPair;
+import java.time.Duration;
+import java.util.Date;
 
 public class JwtHelper {
     static {
@@ -18,13 +23,14 @@ public class JwtHelper {
         key  = null;
     }
     
-    private JwtHelper() { /* provate constructor to avoid instantiation */ }
+    private JwtHelper() { /* private constructor to avoid instantiation */ }
     
     public static String token(final String id, final String subject) {
         return Jwts.builder()
+            .signWith( RS256, PRIV )
             .setId( id )
             .setSubject( subject )
-            .signWith( RS256, PRIV )
+            .setExpiration( new Date(now().plus(EXPIRATION).toEpochMilli()) )
             .compact();
     }
 
@@ -33,7 +39,13 @@ public class JwtHelper {
             .setSigningKey( PUB )
             .parseClaimsJws( token );
     }
+
+    public static Duration expiration() {
+        return EXPIRATION;
+    }
     
-    private static final Key PRIV;
-    private static final Key PUB; 
+    private static final long     DEFAULT_EXPIRATION = 5;  // in seconds
+    private static final Duration EXPIRATION         = ofSeconds(DEFAULT_EXPIRATION);
+    private static final Key      PRIV;
+    private static final Key      PUB; 
 }
--- src/test/java/org/jomoespe/lab/jwt/sample/JwtHelperTest.java
+++ src/test/java/org/jomoespe/lab/jwt/sample/JwtHelperTest.java
@@ -1,11 +1,17 @@
 package org.jomoespe.lab.jwt.sample;
 
-import static org.jomoespe.lab.jwt.sample.JwtHelper.*;
+import static org.jomoespe.lab.jwt.sample.JwtHelper.token;
+import static org.jomoespe.lab.jwt.sample.JwtHelper.claims;
+import static org.jomoespe.lab.jwt.sample.JwtHelper.expiration;
 
-import static org.junit.Assert.*;
+import static java.lang.Thread.currentThread;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.ExpiredJwtException;
 
 import org.junit.Test;
 
@@ -15,9 +21,21 @@ public class JwtHelperTest {
         String token = token("user_id@server.com", "Firstname Lastname");
         assertNotNull(token);
         
-        Jws<Claims> claims = claims(token);
+        Jws<Claims> claims = claims( token );
         assertNotNull(claims);
-        assertEquals("user_id@server.com",   claims.getBody().getId());
+        assertEquals("user_id@server.com",  claims.getBody().getId());
         assertNotNull("Firstname Lastname", claims.getBody().getSubject());
+    }
+
+    @Test(expected = ExpiredJwtException.class)
+    public void assertTokenExpires() {
+        String token = token("user_id@server.com", "Firstname Lastname");
+        assertNotNull(token);
+        
+        try {
+            currentThread().sleep( expiration().getSeconds() * 1000 );
+        } catch (InterruptedException e) {} 
+        
+        Jws<Claims> claims = claims( token );
     }
 }
